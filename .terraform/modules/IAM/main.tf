ad8280db065d5c4ff55ddb37ffd4b7f365eceb25
Koren Peretz <korenperets7@gmail.com>
2025-01-24 16:27:29 +0200

    Targil 3

--- .terraform/modules/IAM/main.tf
+++ .terraform/modules/IAM/main.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy" "main" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = ["s3:ListBucket"]
+        Action   = ["s3:ListBucket"]
         Effect   = "Allow"
         Resource = var.bucket_arn
       }
